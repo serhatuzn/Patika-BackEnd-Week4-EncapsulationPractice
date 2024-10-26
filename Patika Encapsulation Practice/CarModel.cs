4a5ef69cdbfc33c8c68e408f3b12d2edc06fa663
Serhatuzn <seerhatuzun@gmail.com>
2024-10-27 00:19:38 +0300

    Update CarModel.cs

--- Patika Encapsulation Practice/CarModel.cs
+++ Patika Encapsulation Practice/CarModel.cs
@@ -11,26 +11,27 @@ namespace Patika_Encapsulation_Practice
         public string Marka { get; set; }
         public string Model { get; set; }
         public string Renk { get; set; }
-        public int Kapı
+        public int Kapı // Kapı sayısı için özellik, kapsülleme uygulanmıştır
         {
             get
             {
-                return _kapı;
+                return _kapı;  // kapı sayısını döndür.
             }
             set
             {
+                // Eğer kapı sayısı 2 veya 4 dışında bir değer ise
                 if (value < 2 || value > 4)
                 {
-                    Console.WriteLine("Lütfen geçerli bir kapı sayısı giriniz. Kapı sayısı 2 veya 4 olmalıdır.");
-                    _kapı = -1;
+                    Console.WriteLine("Lütfen geçerli bir kapı sayısı giriniz. Kapı sayısı 2 veya 4 olmalıdır."); // hata mesajı gönder.
+                    _kapı = -1; // kapı sayısını -1 yapıyoruz.
                 }
                 else
                 {
-                    _kapı = value;
+                    _kapı = value;  // Geçerli bir değer ise kapı sayısını ata
                 }
             }
         }
-
+        // Constructor, araba nesnesini oluşturmak için kullanılır
         public CarModel(String marka, string model, string renk, int kapı)
         {
             this.Marka = marka;
@@ -39,7 +40,7 @@ namespace Patika_Encapsulation_Practice
             this.Kapı = kapı;
         }
 
-        public void Yazdır()
+        public void Yazdır() // Araba bilgilerini yazdırmak için.
         {
             Console.WriteLine($"Arabanın Markası: {Marka} || Arabanın Modeli: {Model} || Arabanın Renki : {Renk} || Kapı Sayısı: {Kapı}");
         }
